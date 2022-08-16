5a7716fb4e0f6071a752c35db4c81b5c6ed1f9ff
hosamallemon <hosamallemon57@gmail.com>
2022-08-17 00:05:24 +0300

    update on routing and add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/company/login/login.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { CompanyProfileComponent } from './components/company/company-profile/company-profile.component';
+import { VolunteerProfileComponent } from './components/volunteer/volunteer-profile/volunteer-profile.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { CompanyLayoutComponent } from './layout/company-layout/company-layout.component';
 import { VolunteerLayoutComponent } from './layout/volunteer-layout/volunteer-layout.component';
 
 const routes: Routes = [
   {path:'',component:AppComponent},
-  {path:'company',component: CompanyLayoutComponent ,children:[
-    {path:'',redirectTo:'/company/login',pathMatch:'full'},
+  {path:'',component: VolunteerLayoutComponent ,children:[
+    {path:'',redirectTo:'/volunteer' ,pathMatch:'full'},
+    {path:'volunteer',component:VolunteerProfileComponent}
+  ]},
+  {path:'',component: CompanyLayoutComponent ,children:[
+    {path:'',redirectTo:'/company',pathMatch:'full'},
+    {path:'company',component:CompanyProfileComponent},
+  ]},
+
+  {path:'',component: AuthLayoutComponent ,children:[
+    {path:'',redirectTo:'/login',pathMatch:'full'},
     {path:'login',component:LoginComponent}
   ]},
+  {path:'',component: AuthLayoutComponent ,children:[
+    {path:'',redirectTo:'/register',pathMatch:'full'},
+    {path:'register',component:RegisterComponent}
+  ]},
+
+  {path:'company',component: CompanyLayoutComponent ,children:[
+    {path:'',redirectTo:'/company/profile',pathMatch:'full'},
+    {path:'profile',component:CompanyProfileComponent},
+  ]},
+
   {path:'volunteer',component: VolunteerLayoutComponent ,children:[
-    {path:'',redirectTo:'/volunteer/login' ,pathMatch:'full'},
-    {path:'login',component:LoginComponent}
+    {path:'',redirectTo:'/volunteer/profile' ,pathMatch:'full'},
+    {path:'profile',component:VolunteerProfileComponent}
   ]},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyLayoutComponent } from './layout/company-layout/company-layout.component';
 import { VolunteerLayoutComponent } from './layout/volunteer-layout/volunteer-layout.component';
-import { LoginComponent } from './components/company/login/login.component';
-
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { CompanyProfileComponent } from './components/company/company-profile/company-profile.component';
+import { VolunteerProfileComponent } from './components/volunteer/volunteer-profile/volunteer-profile.component';
 @NgModule({
   declarations: [
     AppComponent,
     CompanyLayoutComponent,
     VolunteerLayoutComponent,
-    LoginComponent
+    AuthLayoutComponent,
+    RegisterComponent,
+    CompanyProfileComponent,
+    VolunteerProfileComponent
   ],
   imports: [
     BrowserModule,
