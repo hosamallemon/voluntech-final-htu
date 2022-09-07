a4932dc109da1a6c1077b144b9c927b80b62467b
hosamallemon <hosamallemon57@gmail.com>
2022-09-07 23:46:14 +0300

    server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
-app.use(express.static(__dirname+'/dist/startuptech'));
+
+app.use(express.static(__dirname+'/dist/voluntech'));
 
 app.get('/*', (req, res)=> {
     res.sendFile(__dirname+'/dist/voluntech/index.html');
