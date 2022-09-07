d95f9e07798f0ad6b7e7e0c84276f6f3b0c2e2a0
hosamallemon <hosamallemon57@gmail.com>
2022-09-07 22:43:09 +0300

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname+'/dist/startuptech'));
+
+app.get('/*', (req, res)=> {
+    res.sendFile(__dirname+'/dist/startuptech/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
--- src/app/service/picture-upload/pic-upload.service.ts
+++ src/app/service/picture-upload/pic-upload.service.ts
@@ -3,6 +3,8 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AuthService } from '../authServ/auth.service';
 import { finalize } from 'rxjs';
 import { CompanyService } from './../companyServ/company.service';
+import { Observable, map, take, from, filter } from 'rxjs';
+
 
 
 @Injectable({
@@ -16,11 +18,16 @@ export class PicUploadService {
     const filePath = `$'profile_images/${image.name}`;
     const storageRef = this.fireStorage.ref(filePath);
     const uploadTask = this.fireStorage.upload(filePath, image);
-    return uploadTask.snapshotChanges().pipe(
+    return uploadTask
+    .snapshotChanges()
+    .pipe(
+      // take(1),
       finalize(() => {
         storageRef.getDownloadURL()
         .subscribe((data)=> {
-            this.authService.userState$.subscribe((user)=> {
+            this.authService.userState$
+            .pipe(take(1))
+            .subscribe((user)=> {
               if(user){
                 user?.updateProfile( {
                   photoURL: data
